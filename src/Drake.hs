b026fe903a2296b72a0f0fb46de2b0649cc20905
Nolrai <chrisaupshaw@gmail.com>
2021-03-13 04:16:21 -0800

    Comonads finished

--- src/Drake.hs
+++ src/Drake.hs
@@ -17,18 +17,19 @@ module Drake
 where
 
 import Control.Comonad
-import Control.Monad
-import Data.List (intercalate)
-import Data.Monoid (Any)
+-- import Control.Monad
+-- import Data.List (intercalate)
+-- import Data.Monoid (Any)
 import Data.Vector as V
-import Diagrams.Backend.CmdLine
-import Diagrams.Backend.SVG
-import Diagrams.Core.Compile
-import Diagrams.Core.Types
-import Diagrams.Prelude hiding (after)
-import Diagrams.TwoD
-import Diagrams.TwoD.Layout.Grid
-import System.Random
+
+-- import Diagrams.Backend.CmdLine
+-- import Diagrams.Backend.SVG
+-- import Diagrams.Core.Compile
+-- import Diagrams.Core.Types
+-- import Diagrams.Prelude hiding (after)
+-- import Diagrams.TwoD
+-- import Diagrams.TwoD.Layout.Grid
+-- import System.Random
 
 projectName :: String
 projectName = "Drake"
@@ -54,23 +55,24 @@ instance Comonad RingZipper where
 
 data RingZipper a = RingZipper {front :: Int, vector :: V.Vector a}
 
-data TorusZipper a = TorusZipper {frontT :: (Int, Int), width :: Int, vectorT :: V.Vector a}
+data TorusZipper a = TorusZipper {frontT :: (Int, Int), widthT :: Int, vectorT :: V.Vector a}
 
 instance Functor TorusZipper where
   fmap f t@TorusZipper {vectorT} = t {vectorT = fmap f vectorT}
 
+duplicate'' :: TorusZipper a -> TorusZipper (TorusZipper a)
 duplicate'' t@TorusZipper {..} =
   t {vectorT = V.generate s (\i -> t {frontT = f i})}
   where
     f i = (x i, y i)
-    x i = (i + fst frontT) `mod` width
-    y i = ((i `div` width) + snd frontT) `mod` (s `div` width)
+    x i = (i + fst frontT) `mod` widthT
+    y i = ((i `div` widthT) + snd frontT) `mod` (s `div` widthT)
     s = V.length vectorT
 
 read2d :: TorusZipper a -> (Int, Int) -> a
 TorusZipper {..} `read2d` (i, j) =
-  vectorT ! (((i + fst frontT) `mod` width) + ((j + snd frontT) `mod` (V.length vectorT `div` width)) * width)
+  vectorT ! (((i + fst frontT) `mod` widthT) + ((j + snd frontT) `mod` (V.length vectorT `div` widthT)) * widthT)
 
 instance Comonad TorusZipper where
-  extract r = r `read2d` (0, 0)
+  extract TorusZipper {..} = V.head vectorT
   extend f t = fmap f (duplicate'' t)
