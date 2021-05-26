227b06af868512ccb4db45b722076cb9b7371c9a
Chris A. Upshaw <chrisaupshaw@gmail.com>
2021-05-26 02:40:46 -0700

    refactor: split STCA up and make tests complile 2

---
+++ test/STCASpec.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module STCASpec
+  ( spec,
+  )
+where
+
+import Control.Comonad ()
+import Data.Text ()
+import Drake ()
+import Prelude
+import Test.Hspec
+import Test.Hspec.Golden ()
+import Test.QuickCheck ()
+
+spec :: Spec
+spec = undefined
