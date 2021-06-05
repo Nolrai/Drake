d42ad9a63894b7efc7351737aeb93c11dcd66d9d
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-06-05 14:16:13 -0700

    Implemented Tests for Dist, most fail

--- src/Dist.hs
+++ src/Dist.hs
@@ -1,46 +1,27 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Dist (distSize, fromVector, drawFrom, fromSet, Dist ()) where
+module Dist (distSize, fromVector, drawFrom, Dist.fromSet, Dist (), simplify, convex, histogram, Dist.null) where
 
+import Control.Lens hiding (index)
 import Control.Applicative as App
   ( Alternative (empty),
-    Applicative (pure),
   )
-import Data.IntMap as IntMap
-  ( IntMap,
-    insert,
-    lookupGE,
-    lookupMax,
-    mapKeysWith,
-  )
-import Data.Map as Map (Map, foldlWithKey', insertWith)
+import Data.IntMap.Strict as IntMap
+import Data.IntSet as IntSet
+import Data.Ratio ((%))
+import Data.Map.Strict as Map
 import Data.Set as Set
 import Data.Vector as Vector
 import Relude
-  ( Eq ((==)),
-    Functor,
-    Int,
-    Monoid (mempty),
-    Num ((*), (+)),
-    Ord,
-    Read,
-    Show,
-    error,
-    fst,
-    maybe,
-    snd,
-    (.),
-    (>),
-  )
 import System.Random.Stateful
   ( StatefulGen,
     UniformRange (uniformRM),
   )
+import Test.QuickCheck as QuickCheck hiding (scale)
 
 newtype Dist a = Dist {toIntMap :: IntMap a}
   deriving newtype (Show, Read, Functor)
@@ -51,37 +32,79 @@ instance Eq a => Eq (Dist a) where
       f (Dist intMapX) y =
         let scale = distSize y
          in if scale > 0
-              then mapKeysWith (error "overlapping key map") (* distSize y) intMapX
+              then IntMap.mapKeysWith (error "overlapping key map") (* distSize y) intMapX
               else intMapX
+
+null :: Dist a -> Bool
+null d = distSize d == 0 
 
 distSize :: Dist a -> Int
 distSize = distSize' . toIntMap
 
-distSize' :: IntMap b -> Int
+distSize' :: IntMap a -> Int
 distSize' = maybe 0 fst . IntMap.lookupMax
 
 fromSet :: forall a. Ord a => Set a -> Dist a
-fromSet l = fromSizeMap sizeMap
+fromSet l = Dist $ sizeMap ^. from histogram
   where
     sizeMap :: Map a Int
     sizeMap = Set.foldl' (\m x -> Map.insertWith (+) x 1 m) mempty l
 
 fromVector :: forall a. Ord a => Vector a -> Dist a
-fromVector l = fromSizeMap sizeMap
+fromVector l = Dist $ sizeMap ^. from histogram
   where
     sizeMap :: Map a Int
     sizeMap = Vector.foldl' (\m x -> Map.insertWith (+) x 1 m) mempty l
-
-fromSizeMap :: forall a. Map a Int -> Dist a
-fromSizeMap m = Dist (toIntMapF m)
-  where
-    toIntMapF :: Map a Int -> IntMap a
-    toIntMapF = Map.foldlWithKey' toSumMap (mempty :: IntMap a)
-    toSumMap :: IntMap b -> b -> Int -> IntMap b
-    toSumMap sofar b multiplicity = let oldKey = distSize' sofar in IntMap.insert (oldKey + multiplicity) b sofar
 
 drawFrom :: (Alternative m, StatefulGen g m) => g -> Dist a -> m a
 drawFrom g d = do
   index <- uniformRM (1, distSize d) g
   let result = IntMap.lookupGE index (toIntMap d)
   maybe App.empty (pure . snd) result
+
+histogram :: forall a. Ord a => Iso' (IntMap a) (Map a Int)
+histogram = iso toH fromH
+  where
+    toH :: IntMap a -> Map a Int
+    toH im = snd $ f (0, mempty :: Map a Int) im
+    f :: (Key, Map a Int) -> IntMap a -> (Key, Map a Int)
+    f = IntMap.foldlWithKey' fromSumMap
+    fromSumMap :: (Key, Map a Int) -> Key -> a -> (Key, Map a Int)
+    fromSumMap (top, sofar) k a = (k, Map.insertWith (+) a (k - top) sofar)
+    fromH :: Map a Int -> IntMap a
+    fromH = Map.foldlWithKey' toSumMap (mempty :: IntMap a)
+    toSumMap :: IntMap a -> a -> Int -> IntMap a
+    toSumMap sofar b multiplicity = let oldKey = distSize' sofar in IntMap.insert (oldKey + multiplicity) b sofar
+
+convex :: Ord a => Int -> Dist a -> Int -> Dist a -> Dist a 
+convex alpha a' beta b' = simplify $ Dist (stack newA newB)
+  where
+    a = simplify a'
+    b = simplify b'
+    aSize = distSize a
+    bSize = distSize b
+    yOverX = (aSize % bSize) / (alpha % beta)
+    y = numerator yOverX
+    x = denominator yOverX
+    newA = IntMap.mapKeysMonotonic (* y) (toIntMap a)
+    newB = IntMap.mapKeysMonotonic (* x) (toIntMap b)
+
+stack :: Ord a => IntMap a -> IntMap a -> IntMap a
+stack a b = Map.unionWith (+) (a ^. histogram) (b ^. histogram) ^. from histogram 
+
+simplify :: Dist a -> Dist a 
+simplify (Dist im) = 
+  let (gcd' :: Int) = IntSet.foldl' gcd 0 (IntMap.keysSet im) in
+  Dist $
+    if gcd' > 1
+      then IntMap.mapKeys (`div` gcd') im
+      else im
+
+instance Arbitrary a => Arbitrary (Dist a) where
+  arbitrary = Dist . IntMap.mapKeys toPos <$> arbitrary
+  shrink (Dist im) = 
+    Dist . IntMap.mapKeys toPos <$>
+      shrink (IntMap.mapKeysMonotonic (\ k -> k - 1) im)
+
+toPos :: Key -> Key
+toPos i = abs i + 1
--- test/DistSpec.hs
+++ test/DistSpec.hs
@@ -16,17 +16,19 @@ import GHC.Real ((%))
 import System.Random
 import System.Random.Stateful
 import Test.Hspec
-import Test.QuickCheck
+import Test.QuickCheck hiding (scale)
+import Control.Lens.Properties
+import Control.Lens
 import Prelude hiding (die)
 
 vectorToHistogram :: Ord a => Vector a -> Histogram a
 vectorToHistogram = Histogram.fromList . Vector.toList
 
-dieSides :: Vector Char
-dieSides = ['A' .. 'E']
+dieSides :: Set Char
+dieSides = Set.fromAscList ['A' .. 'E']
 
 die :: Dist Char
-die = fromVector dieSides
+die = fromSet dieSides
 
 rootNumTrials :: Int
 rootNumTrials = 100
@@ -35,9 +37,10 @@ numTrials :: Int
 numTrials = rootNumTrials * rootNumTrials
 
 testKey :: Histogram Char -> Char -> Bool
-testKey h key = (key, roundOff' (Histogram.lookup key h)) == (key, roundOff' (numTrials `div` Prelude.length dieSides))
-  where
-    roundOff' original = roundOff original rootNumTrials
+testKey h key = (key, roundOffTrials (Histogram.lookup key h)) == (key, roundOffTrials (numTrials `div` Prelude.length dieSides))
+
+roundOffTrials :: Int -> Int
+roundOffTrials original = roundOff original rootNumTrials
 
 roundOff :: Int -> Int -> Int
 roundOff original scale = round (original % scale) * scale
@@ -48,7 +51,10 @@ isApproximatelyUniform good dist =
     g <- newAtomicGenM =<< getStdGen
     v <- Vector.replicateM numTrials (g `drawFrom` dist)
     let should = (if good then shouldSatisfy else shouldNotSatisfy) :: Histogram Char -> (Histogram Char -> Bool) -> Expectation
-    vectorToHistogram v `should` \h -> Vector.all (testKey h) dieSides
+    vectorToHistogram v `should` \h -> setAll (testKey h) dieSides
+
+setAll :: (a -> Bool) -> Set a -> Bool
+setAll p = Set.foldr (\ item sofar -> p item && sofar) True
 
 spec :: Spec
 spec = focus $
@@ -57,10 +63,42 @@ spec = focus $
       it "should detect bad dice" . property $
         \l ->
           do
-            let toDieSide a = dieSides Vector.! (abs a `mod` Vector.length dieSides)
+            let toDieSide a = chr $ ord (Set.findMin dieSides) + (abs a `mod` Set.size dieSides)
             let v = Vector.fromList l
-            let testDie = fromVector $ (toDieSide <$> v) <> dieSides
+            let testDie = fromVector $ (toDieSide <$> v) <> Vector.fromList (Set.toList dieSides)
             testDie /= die ==> isApproximatelyUniform False testDie
-    describe "drawFrom . fromVector" $ do
-      it "shouldbe isApproximatelyUniform uniform" $
+    describe "drawFrom . fromSet" $ do
+      it " isApproximatelyUniform" $
         isApproximatelyUniform True die
+    describe "simplify" $ do
+      it "should produce eq Dist" . property $
+        \ (d :: Dist Char) -> simplify d == d
+    describe "convex" $ do
+      it "is idempotent" . property $
+        \ (d :: Dist Char) (Positive alpha) (Positive beta) -> convex alpha d beta d `shouldBe` d
+      it "depnds only on the ratio" . property $
+        \ (Positive alpha) (a :: Dist Char) (Positive beta) b (Positive gamma) -> 
+          convex (gamma * alpha) a (gamma * beta) b `shouldBe` convex alpha a beta b
+      it "on n 0 it = a" . property $
+        \ (Positive alpha) (a :: Dist Char) b -> 
+          convex alpha a 0 b `shouldBe` a
+      it "on 0 n it = b" . property $
+        \ (a :: Dist Char) (Positive beta) b -> 
+          convex 1 a beta b `shouldBe` b
+      it "is communative on scale/value pairs" . property $
+        \ (Positive alpha) (a :: Dist Char) (Positive beta) b -> 
+          convex alpha a beta b `shouldBe` convex beta b alpha a
+      it "should result in a results ratio of alpha % beta" . property $
+        \ (Positive alpha) (a :: Dist Char) (Positive beta) b -> 
+          not (Dist.null a) && not (Dist.null b) ==>
+            do
+              let (combo :: Dist (Either Char Char)) = 
+                    convex alpha (Left <$> a) beta (Right <$> b)
+              g <- newAtomicGenM =<< getStdGen
+              v <- Vector.replicateM numTrials (g `drawFrom` combo)
+              let h = vectorToHistogram (isLeft <$> v)
+              roundOffTrials (Histogram.lookup True h) `shouldBe` 
+                roundOffTrials ((numTrials * alpha) `div` (alpha + beta))
+
+    describe "histogram" $ do
+      it "is an Iso" $ isIso (histogram :: Iso' (IntMap Char) (Map Char Int))
