85d60ca0ee80c44f0c154583f8f1558ed0f92b02
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-06-09 22:38:57 -0700

    Change the Direction type to handle Hexagonal offsets

--- src/Hex/Direction.hs
+++ src/Hex/Direction.hs
@@ -9,26 +9,44 @@ module STCA.Direction (Direction (..), allDirections, inv, offset, rotateClockwi
 import Relude (Enum, Eq, Generic, Int, Num ((+), (-)), Ord, Read, Show)
 
 -- the vonNeumann neihboorhood
-data Direction = N | E | S | W
+data Direction 
+  = YZ -- +Y -Z i.e. N
+  | XZ -- +X -Z i.e. NE
+  | XY -- and so on, going clockwse
+  | ZY
+  | ZX
+  | YX
   deriving stock (Show, Read, Eq, Ord, Enum, Generic)
 
 allDirections :: [Direction]
-allDirections = [N, E, S, W]
+allDirections = 
+  [YZ
+  , XZ
+  , XY
+  , ZY
+  , ZX
+  , YX
+  ]
 
 inv :: Direction -> Direction
-inv N = S
-inv E = W
-inv S = N
-inv W = E
+inv YZ = ZY
+inv XZ = ZX
+inv XY = YX
+inv ZY = YZ
+inv ZX = XZ
+inv YX = XY
 
 offset :: (Int, Int) -> Direction -> (Int, Int)
-offset (x, y) N = (x, y + 1)
-offset (x, y) E = (x + 1, y)
-offset (x, y) S = (x, y - 1)
-offset (x, y) W = (x - 1, y)
+offset (x, y) XZ = (x+1,y)
+offset (x, y) XY = (x+1,y-1)
+offset (x, y) ZY = (x,y-1)
+offset (x, y) ZX = (x-1,y)
+offset (x, y) YX = (x-1,y+)
 
 rotateClockwise :: Direction -> Direction
-rotateClockwise N = E
-rotateClockwise E = S
-rotateClockwise S = W
-rotateClockwise W = N
+rotateClockwise YZ = XZ 
+rotateClockwise XZ = XY
+rotateClockwise XY = ZY
+rotateClockwise ZY = ZX
+rotateClockwise ZX = YX
+rotateClockwise YX = YZ
